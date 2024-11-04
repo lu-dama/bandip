803aa53dcc8626676eb878274f17a7231f4effa7
鹿小汐 <54878228+lu-dama@users.noreply.github.com>
2024-11-04 22:04:50 +0800

    Update band_ip.sh

--- band_ip.sh
+++ band_ip.sh
@@ -1,4 +1,22 @@
-#/bin/bash
-interfaces=enp3s0f1
-for I in $(seq 34 62); 
- do nmcli con mod ${interfaces} +ipv4.addresses X.X.X.${I}; done; nmcli con up ${interfaces}
+#!/bin/bash
+
+# 网络接口名称
+INTERFACE="eth0"
+
+# 添加IP的起始地址和结束地址
+START_IP=2
+END_IP=30
+
+# 子网前缀
+SUBNET="1.1.1."
+PREFIX="/27"
+
+# 使用for循环批量添加IP
+for i in $(seq $START_IP $END_IP); do
+  IP="${SUBNET}${i}${PREFIX}"
+  echo "Adding IP: $IP"
+  nmcli connection modify $INTERFACE +ipv4.addresses $IP
+done
+
+# 重启网络接口
+nmcli connection down $INTERFACE && nmcli connection up $INTERFACE
